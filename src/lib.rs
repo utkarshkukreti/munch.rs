668d561a29c1389af65afd5fb4a48405e8b2f947
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2017-02-10 00:55:07 +0530

    feat(Parser)

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 pub type Result<Output, Error> = std::result::Result<(usize, Output), (usize, Error)>;
+
+pub trait Parser<Input> {
+    type Output;
+    type Error;
+
+    fn parse(&mut self, input: Input, from: usize) -> Result<Self::Output, Self::Error>;
+}
