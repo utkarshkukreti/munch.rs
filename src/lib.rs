b7080b1f37286940e208ee1421e3df055e008ee3
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2017-02-10 12:24:09 +0530

    feat({Or,Parser::or})

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,13 @@ pub trait Parser<Input> {
     {
         And(self, b)
     }
+
+    fn or<B>(self, b: B) -> Or<Self, B>
+        where Self: Sized,
+              B: Parser<Input, Output = Self::Output, Error = Self::Error>
+    {
+        Or(self, b)
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -45,5 +52,23 @@ impl<A, B, Input> Parser<Input> for And<A, B>
         let (from, a) = self.0.parse(input, from)?;
         let (from, b) = self.1.parse(input, from)?;
         Ok((from, (a, b)))
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Or<A, B>(pub A, pub B);
+
+impl<A, B, Input> Parser<Input> for Or<A, B>
+    where A: Parser<Input>,
+          B: Parser<Input, Output = A::Output, Error = A::Error>,
+          Input: Copy
+{
+    type Output = A::Output;
+    type Error = A::Error;
+
+    fn parse(&mut self, input: Input, from: usize) -> Result<Self::Output, Self::Error> {
+        self.0
+            .parse(input, from)
+            .or_else(|_| self.1.parse(input, from))
     }
 }
